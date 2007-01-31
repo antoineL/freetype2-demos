8776560a018990eb80779b57831252f65a844cee
Werner Lemberg <wl@gnu.org>
2007-01-31 08:53:59 +0000

    * Version 2.3.1 released. =========================
    Tag sources with `VER-2-3-1-FINAL'.
    
    * README: Updated.

--- src/ftvalid.c
+++ src/ftvalid.c
@@ -2,7 +2,7 @@
 /*                                                                          */
 /*  The FreeType project -- a free and portable quality font engine         */
 /*                                                                          */
-/*  Copyright 2005, 2006 by                                                 */
+/*  Copyright 2005, 2006, 2007 by                                           */
 /*  D. Turner, R.Wilhelm, and W. Lemberg                                    */
 /*                                                                          */
 /*  ftvalid: Validates layout related tables of OpenType and                */

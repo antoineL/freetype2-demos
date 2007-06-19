11e47b6418be3f1bc2d26790760fedd76944693a
Werner Lemberg <wl@gnu.org>
2007-06-19 04:47:01 +0000

    * src/ftview.c (usage): Make it more readable.

--- src/ftview.c
+++ src/ftview.c
@@ -2,7 +2,7 @@
 /*                                                                          */
 /*  The FreeType project -- a free and portable quality TrueType renderer.  */
 /*                                                                          */
-/*  Copyright 1996-2000, 2003, 2004, 2005, 2006 by                          */
+/*  Copyright 1996-2000, 2003, 2004, 2005, 2006, 2007 by                    */
 /*  D. Turner, R.Wilhelm, and W. Lemberg                                    */
 /*                                                                          */
 /*                                                                          */
@@ -938,17 +938,27 @@ Next:
     fprintf( stderr,  "ftview: simple glyph viewer -- part of the FreeType project\n" );
     fprintf( stderr,  "-----------------------------------------------------------\n" );
     fprintf( stderr,  "\n" );
-    fprintf( stderr,  "Usage: %s [status below] ppem fontname[.ttf|.ttc] ...\n",
+    fprintf( stderr,  "Usage: %s [options] pt font ...\n",
              execname );
     fprintf( stderr,  "\n" );
-    fprintf( stderr,  "  -r R      use resolution R dpi (default: 72 dpi)\n" );
-    fprintf( stderr,  "  -f index  specify first index to display\n" );
-    fprintf( stderr,  "  -e enc    specify encoding tag (default: no encoding)\n" );
-    fprintf( stderr,  "  -D        dump cache usage statistics\n" );
-    fprintf( stderr,  "  -m text   use `text' for rendering\n" );
-    fprintf( stderr,  "  -l nn     change rendering mode (0 <= nn <= %d)\n",
+    fprintf( stderr,  "  pt        The point size for the given resolution.\n" );
+    fprintf( stderr,  "            If resolution = 72 dpi, this directly gives the\n" );
+    fprintf( stderr,  "            ppem value (pixels per EM).\n" );
+    fprintf( stderr,  "  font      The font file(s) to display; if no extension is given,\n" );
+    fprintf( stderr,  "            ftview tries to append one of the following suffixes:\n" );
+    fprintf( stderr,  "            `.ttf', `.ttc', `.otf', `.pfa', `.pfb'.\n" );
+    fprintf( stderr,  "            For `.pfa' and `.pfb' files, ftview also tries to\n" );
+    fprintf( stderr,  "            attach the corresponding metrics file (with extension\n" );
+    fprintf( stderr,  "            `.afm' or `.pfm').\n" );
+    fprintf( stderr,  "\n" );
+    fprintf( stderr,  "  -r R      Use resolution R dpi (default: 72 dpi).\n" );
+    fprintf( stderr,  "  -f index  Specify first index to display.\n" );
+    fprintf( stderr,  "  -e enc    Specify encoding tag (default: no encoding).\n" );
+    fprintf( stderr,  "  -D        Dump cache usage statistics.\n" );
+    fprintf( stderr,  "  -m text   Use `text' for rendering.\n" );
+    fprintf( stderr,  "  -l nn     Change rendering mode (0 <= nn <= %d).\n",
              N_LCD_MODES );
-    fprintf( stderr,  "  -p        preload file in memory, simulates memory-mapped file\n" );
+    fprintf( stderr,  "  -p        Preload file in memory to simulate memory-mapping.\n" );
     fprintf( stderr,  "\n" );
 
     exit( 1 );
